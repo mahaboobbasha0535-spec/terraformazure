3917515c8f66656673528657e10f26006bc0db91
Mahaboob Basha <mahaboob.basha0535@gmail.com>
2025-10-28 00:14:55 +0530

    subnet added

--- vnet.tf
+++ vnet.tf
@@ -4,3 +4,11 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   location = "westus"
 }
+
+resource "azurerm_subnet" "subnet" {
+    name = "subnet01"
+    address_prefixes = [ "10.1.1.0/24" ]
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+  
+}
